1586499d2d970c614f6eb16a401c4fe995b87930
johnpmeyers20 <johnpmeyers20@yahoo.com>
2021-05-27 13:36:50 -0400

    added all functionality found on scrimba

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const saveBtn = document.getElementById("save-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const ulEl = document.getElementById("ul-el")
 
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLeads(myLeads)
+}
+
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  renderLeads()
 })
 
+saveBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads);
+  })
+})
+
+deleteBtn.addEventListener("dblclick", function () {
+  myLeads = []
+  localStorage.removeItem("myLeads")
+  renderLeads(myLeads)
+})
+ 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
